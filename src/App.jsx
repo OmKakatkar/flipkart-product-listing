fade2adae9056e54e964f58574fb0de3880229f5
Om Kakatkar <omkakatkar@gmail.com>
2022-08-24 20:00:39 +0530

    [FEAT] Add Header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Header } from "./components";
+
+function App() {
+	return (
+		<div className="app">
+			{/* TODO: Add header, filter sidebar, products */}
+			<Header />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,58 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faAngleDown,
+	faCartShopping,
+	faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+import "./header.css";
+
+export function Header() {
+	return (
+		<header className="header">
+			<div className="inline-flex logo">
+				<img
+					src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png"
+					alt="flipkart logo"
+					className="logo-image"
+				/>
+				<div>
+					<span className="logo-text">
+						Explore <span>Plus</span>
+					</span>
+					<img
+						src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/plus_aef861.png"
+						alt=""
+						className="logo-icon"
+					></img>
+				</div>
+			</div>
+			<div className="inline-flex search">
+				<input
+					type="search"
+					placeholder="Search for products, brands and more"
+				/>
+				<FontAwesomeIcon
+					icon={faSearch}
+					className="search-icon"
+				></FontAwesomeIcon>
+			</div>
+			<nav className="inline-flex navigation">
+				<button>Login</button>
+				<div>
+					<span>More</span>
+					<FontAwesomeIcon
+						icon={faAngleDown}
+						className="more-icon"
+					></FontAwesomeIcon>
+				</div>
+				<div>
+					<FontAwesomeIcon
+						icon={faCartShopping}
+						className="cart-icon"
+					></FontAwesomeIcon>
+					<span>Cart</span>
+				</div>
+			</nav>
+		</header>
+	);
+}
